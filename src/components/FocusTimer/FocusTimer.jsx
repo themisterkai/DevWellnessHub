2cba7488eaa514dc0084c7755474d4409952d3e4
Kris Albert Lee <krisalbert@gmail.com>
2023-11-27 18:20:45 +0100

    add initial app structure

---
+++ src/components/FocusTimer/FocusTimer.jsx
@@ -0,0 +1,6 @@
+// This is the component shown in the Dashboard
+// Conditionally show either expanded or unexpanded depending the screen size
+export const FocusTimer = () => {
+  // if desktop render <FocusTimerDetailed />
+  // if mobile clicking will go to <MobileFocusTimerPage />
+};
