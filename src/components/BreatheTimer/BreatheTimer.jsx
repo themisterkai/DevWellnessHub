82b97b71d20c796213535bac2c25846214fcd4f4
Fabio <cassisafabio97@gmail.com>
2023-11-28 12:04:49 +0100

    initial commit

--- src/components/BreatheTimer/BreatheTimer.jsx
+++ src/components/BreatheTimer/BreatheTimer.jsx
@@ -1,6 +1,4 @@
 // This is the component shown in the Dashboard
-// Conditionally show either expanded or unexpanded depending the screen size
 export const BreatheTimer = () => {
-  // if desktop render <BreatheTimerDetailed />
-  // if mobile clicking will go to <MobileBreatheTimerPage />
+
 };
--- src/components/FocusTimer/FocusTimer.jsx
+++ src/components/FocusTimer/FocusTimer.jsx
@@ -1,6 +1,5 @@
 // This is the component shown in the Dashboard
-// Conditionally show either expanded or unexpanded depending the screen size
+
 export const FocusTimer = () => {
-  // if desktop render <FocusTimerDetailed />
-  // if mobile clicking will go to <MobileFocusTimerPage />
+  
 };
