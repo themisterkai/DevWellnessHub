a2699b56bb7c6b3a554cc9e009895c825c7b6c6d
Kris Albert Lee <krisalbert@gmail.com>
2023-11-29 23:13:37 +0100

    udpate App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import { DesktopHomepage } from "./pages/DesktopHomePage";
-import { StartPage } from "./pages/StartPage";
+import { useState } from 'react';
+import { StartPage } from './pages/StartPage';
+import { DesktopHomepage } from './pages/DesktopHomepage';
 
 export const App = () => {
   const [isSetupComplete, setIsSetupComplete] = useState(false);
-  
+
   const handleSetupComplete = () => {
     setIsSetupComplete(true);
   };
 
   return (
     <div>
-    {isSetupComplete ? (
-      <DesktopHomepage />
-    ) : (
-      <StartPage onSetupComplete={handleSetupComplete} />
-    )}
-  </div>
+      {isSetupComplete ? (
+        <DesktopHomepage />
+      ) : (
+        <StartPage onSetupComplete={handleSetupComplete} />
+      )}
+    </div>
   );
 };
