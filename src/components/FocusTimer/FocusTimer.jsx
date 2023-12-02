3779e1e5fe3769d84a7304ef2490885732459e75
Fabio <cassisafabio97@gmail.com>
2023-12-02 12:08:05 +0100

    Dashboard responsiveness + Settings page on both views

--- src/components/FocusTimer/FocusTimer.jsx
+++ src/components/FocusTimer/FocusTimer.jsx
@@ -1,11 +1,18 @@
 // This is the component shown in the Dashboard
+import { FocusCircle, SetFocusText } from "../../assets/SVGElements";
 import "./FocusTimer.css"
 
 export const FocusTimer = () => {
     
     return(
         <div className="focus-wrapper">
-
+                <div className="focus-name">. FOCUS</div>          
+                <div className="focus-timer-done-counter">0</div>
+                <div className="focus-circle">
+                    <FocusCircle />
+                    <SetFocusText />
+                </div>
+                
         </div>
     )
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 //Dashboard that loads in mobile & it's used
 //as a component for Desktop/Tablet
 import { useSelector } from 'react-redux';
-import "./Dashboard.css"
+import { DashLine, SettingsIcon } from "../assets/SVGElements";
 import { FocusTimer } from '../components/FocusTimer/FocusTimer';
 import { HabitTracker } from '../components/HabitTracker/HabitTracker';
 import { MoodTracker } from '../components/MoodTracker/MoodTracker';
 import { BreatheTimer } from '../components/BreatheTimer/BreatheTimer';
+import "./Dashboard.css"
 
 export const Dashboard = () => {
     const settingsState = useSelector(state => state.settings);
@@ -14,13 +15,18 @@ export const Dashboard = () => {
     //Import FocusTimer, Habit,Tracker, MoodTracker, BreatheTimer
     return (
             <div className="dashboard-wrapper">
-                <h1 className="dash-user-name">Welcome {settingsState.name}</h1>
                 <div className="app-container">
+                    <header className="dashboard-header">
+                        <div className="main-app-name">DevWellnessHub</div>
+                        <SettingsIcon /> 
+                    </header>
+                    <h1 className="dash-user-name">Welcome {settingsState.name}, ready for today's session?</h1>
+                    <DashLine />
                     <div className="app-wrapper">
-                        <FocusTimer />
-                        <HabitTracker />
-                        <BreatheTimer />
-                        <MoodTracker />
+                            <FocusTimer />
+                            <HabitTracker />
+                            <BreatheTimer />
+                            <MoodTracker />
                     </div>
                 </div>
             </div>
