a0400c50b2ca1f3f83d139c61771ac31af44fdc4
Kris Albert Lee <krisalbert@gmail.com>
2023-12-06 15:17:14 +0100

    pull historical data from localStorage

--- src/components/MoodTracker/MoodTrackerDetailed.jsx
+++ src/components/MoodTracker/MoodTrackerDetailed.jsx
@@ -12,6 +12,10 @@ export const MoodTrackerDetailed = () => {
   const dispatch = useDispatch();
   const mood = useSelector(state => state.mood);
 
+  const historical = useSelector(state => state.historical);
+
+  console.log('historical:', historical);
+
   const handleUpdateMoodLevel = moodLevel => {
     dispatch(setMoodLevel({ moodLevel }));
     dispatch({ type: 'SAVE_DATA' });
@@ -30,41 +34,42 @@ export const MoodTrackerDetailed = () => {
   return (
     <div className="main-wrapper">
       <div className="app-container">
-      <header className="main-header">
-          <div className="main-app-name">. MOOD</div>    
-      </header>
-      <h2 className="secondary-header">How are you feeling today?</h2>
-      <div className="range-mood">
-        <div>Mood Level: {mood.moodLevel}</div>
-        <input
-          type="range"
-          min={1}
-          max={5}
-          onChange={e => handleUpdateMoodLevel(e.target.value)}
-          value={mood.moodLevel}
-        ></input>
-      </div>
-      <div className="range-mood">
-        <div>Energy Level: {mood.energyLevel}</div>
-        <input
-          type="range"
-          min={1}
-          max={5}
-          onChange={e => handleUpdateEnergyLevel(e.target.value)}
-          value={mood.energyLevel}
-        ></input>
-      </div>
-      <div className="range-mood">
-        <div>Overwhelmed Level: {mood.overwhelmedLevel}</div>
-        <input
-          type="range"
-          min={1}
-          max={5}
-          onChange={e => handleUpdateOverwhelmedLevel(e.target.value)}
-          value={mood.overwhelmedLevel}
-        ></input>
-      </div>
-      <MobileToDashBTN />
+        <header className="main-header">
+          <div className="main-app-name">. MOOD</div>
+        </header>
+        <h2 className="secondary-header">How are you feeling today?</h2>
+        <div className="range-mood">
+          <div>Mood Level: {mood.moodLevel}</div>
+          <input
+            type="range"
+            min={1}
+            max={5}
+            onChange={e => handleUpdateMoodLevel(e.target.value)}
+            value={mood.moodLevel}
+          ></input>
+        </div>
+        <div className="range-mood">
+          <div>Energy Level: {mood.energyLevel}</div>
+          <input
+            type="range"
+            min={1}
+            max={5}
+            onChange={e => handleUpdateEnergyLevel(e.target.value)}
+            value={mood.energyLevel}
+          ></input>
+        </div>
+        <div className="range-mood">
+          <div>Overwhelmed Level: {mood.overwhelmedLevel}</div>
+          <input
+            type="range"
+            min={1}
+            max={5}
+            onChange={e => handleUpdateOverwhelmedLevel(e.target.value)}
+            value={mood.overwhelmedLevel}
+          ></input>
+        </div>
+        <div className="mood-history"></div>
+        <MobileToDashBTN />
       </div>
     </div>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -3,6 +3,11 @@ export const getCurrentDate = () => {
   return currentDate;
 };
 
+export const getYesterdayDate = () => {
+  const yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+  return yesterday;
+};
+
 export const millisToMinutesAndSeconds = millis => {
   var minutes = Math.floor(millis / 60000);
   var seconds = ((millis % 60000) / 1000).toFixed(0);
