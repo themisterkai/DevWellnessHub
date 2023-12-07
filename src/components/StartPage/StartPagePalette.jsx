7156e17cf809bf8ba129f5e86d0ed11a4e3f0128
Fabio <cassisafabio97@gmail.com>
2023-12-07 18:58:14 +0100

    small fix for the palette in starting

--- src/components/StartPage/StartPagePalette.jsx
+++ src/components/StartPage/StartPagePalette.jsx
@@ -33,6 +33,8 @@ export const StartPagePalette = ({ name, page, setPage }) => {
         >
           <option value="dark">Dark</option>
           <option value="light">Light</option>
+          <option value="teal">Teal</option>
+          <option value="earth">Earth</option>
           {/* Add more options for additional color palettes */}
         </select>
         <div className="start-page-button">
