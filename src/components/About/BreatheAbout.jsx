54c65b5160d85ea427c24a694f9bd5c8ea662b01
Kris Albert Lee <krisalbert@gmail.com>
2023-12-07 17:40:42 +0100

    fix routing of mobile pages when we switch to desktop

--- src/components/About/BreatheAbout.jsx
+++ src/components/About/BreatheAbout.jsx
@@ -1,36 +1,49 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { MobileGoBackBTN } from '../MobileBTN';
+import useScreenSize from '../../hooks/useScreenSize';
 import './About.css';
 
 export const BreatheAbout = () => {
+  const navigate = useNavigate();
+  const { isMobile } = useScreenSize();
+
+  useEffect(() => {
+    if (!isMobile) {
+      navigate('/breathe-timer');
+    }
+  }, [isMobile]);
+
   return (
     <div className="about-wrapper">
       <div className="app-container">
-      <div className="main-header">
-      <div className="about-header">. BREATHE</div>
-      </div>
-      <div className="about-paragraph">
-        Mindful breathing is a simple yet transformative practice that taps into
-        the restorative potential of each breath. It serves as an anchor,
-        grounding you in the present moment and offering a respite from the
-        demands of the digital world.
-        <p />
-        <i>Key Benefits:</i>
-        <ul>
-          <li>
-            <b>Stress Reduction:</b> Engage in guided breathing exercises to
-            alleviate stress and tension
-          </li>
-          <li>
-            <b>Improved Focus:</b> Take short breaks with purpose
-          </li>
-          <li>
-            <b>Physical Relaxation:</b> Experience the physical benefits of
-            controlled breathing, such as reduced muscle tension and a slower
-            heart rate
-          </li>
-        </ul>
-      </div>
-      <MobileGoBackBTN />
+        <div className="main-header">
+          <div className="about-header">. BREATHE</div>
+        </div>
+        <div className="about-paragraph">
+          Mindful breathing is a simple yet transformative practice that taps
+          into the restorative potential of each breath. It serves as an anchor,
+          grounding you in the present moment and offering a respite from the
+          demands of the digital world.
+          <p />
+          <i>Key Benefits:</i>
+          <ul>
+            <li>
+              <b>Stress Reduction:</b> Engage in guided breathing exercises to
+              alleviate stress and tension
+            </li>
+            <li>
+              <b>Improved Focus:</b> Take short breaks with purpose
+            </li>
+            <li>
+              <b>Physical Relaxation:</b> Experience the physical benefits of
+              controlled breathing, such as reduced muscle tension and a slower
+              heart rate
+            </li>
+          </ul>
+        </div>
+        <MobileGoBackBTN />
       </div>
     </div>
   );
--- src/components/About/FocusAbout.jsx
+++ src/components/About/FocusAbout.jsx
@@ -1,38 +1,51 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { MobileGoBackBTN } from '../MobileBTN';
+import useScreenSize from '../../hooks/useScreenSize';
 import './About.css';
 
 export const FocusAbout = () => {
+  const navigate = useNavigate();
+  const { isMobile } = useScreenSize();
+
+  useEffect(() => {
+    if (!isMobile) {
+      navigate('/focus-timer');
+    }
+  }, [isMobile]);
+
   return (
     <div className="about-wrapper">
       <div className="app-container">
-      <div className="main-header">
-      <div className="about-header">. FOCUS</div>  
-      </div>
-      <div className="about-paragraph">
-        A Focus Timer, or Pomodoro timer, divides your work into manageable
-        intervals, typically 25 minutes. After each timer, we invite you to take
-        a short break. It&apos;s a structured approach to boost focus and
-        productivity.
-        <p />
-        <i>Key Benefits:</i>
-        <ul>
-          <li>
-            <b>Maximized Productivity:</b> Work in concentrated bursts,
-            minimizing procrastination
-          </li>
-          <li>
-            <b>Enhanced Concentration:</b> Break tasks into intervals to foster
-            deep concentration
-          </li>
-          <li>
-            <b>Improved Time Management:</b> Establish a disciplined routine
-          </li>
-          <li>
-            <b>Reduced Stress:</b> Regular breaks prevent burnout
-          </li>
-        </ul>
-      </div>
-      <MobileGoBackBTN />
+        <div className="main-header">
+          <div className="about-header">. FOCUS</div>
+        </div>
+        <div className="about-paragraph">
+          A Focus Timer, or Pomodoro timer, divides your work into manageable
+          intervals, typically 25 minutes. After each timer, we invite you to
+          take a short break. It&apos;s a structured approach to boost focus and
+          productivity.
+          <p />
+          <i>Key Benefits:</i>
+          <ul>
+            <li>
+              <b>Maximized Productivity:</b> Work in concentrated bursts,
+              minimizing procrastination
+            </li>
+            <li>
+              <b>Enhanced Concentration:</b> Break tasks into intervals to
+              foster deep concentration
+            </li>
+            <li>
+              <b>Improved Time Management:</b> Establish a disciplined routine
+            </li>
+            <li>
+              <b>Reduced Stress:</b> Regular breaks prevent burnout
+            </li>
+          </ul>
+        </div>
+        <MobileGoBackBTN />
       </div>
     </div>
   );
